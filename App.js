bb28f6d1c2dababde8611cb44f61e6234a213a40
Arpit <arpitmalik66@gmail.com>
2020-12-26 14:57:39 +0530

    basic link established

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { GLView } from 'expo-gl';
+import * as THREE from 'three';
+import { Renderer } from 'expo-three';
+
+export default class App extends Component {
+  _onGLContextCreate = async gl => {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000
+    );
+    const renderer = new Renderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+      
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+
+    const cube = new THREE.Mesh(geometry, material);    
+    scene.add(cube);
+    camera.position.z = 2;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+    animate();
+  }
+
+  render () {
+    return (
+      <GLView 
+        style={{ flex: 1 }}
+        onContextCreate={this._onGLContextCreate} />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
